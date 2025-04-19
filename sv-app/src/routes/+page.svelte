a5291c9145a124c42f9e678b5b136f8ad21d7d73
Ankur Seth <iankurseth@gmail.com>
2025-04-19 08:41:10 +0530

    Added some default markup

--- sv-app/src/routes/+page.svelte
+++ sv-app/src/routes/+page.svelte
@@ -3,4 +3,14 @@
 	<title>Tabstar</title>
 </svelte:head>
 
-<h1>Tabstar</h1>
+<section class="">
+	<header class="p-6 bg-base-200">
+		<section class="prose text-center mx-auto">
+			<h4>Tabstar</h4>
+		</section>
+	</header>
+
+	<section class="max-w-3xl mx-auto p-4">
+		<button class="btn btn-primary">Add</button>
+	</section>
+</section>
